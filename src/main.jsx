c7bca89775cbafd761e0b1edc29fdc20aa586333
samd474 <sam.darr@outlook.com>
2024-12-01 11:07:48 -0500

    Refactor index.html to streamline component loading and update ProductList.jsx for improved rendering

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,14 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
 import ProductList from './ProductList.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+    <>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ProductList />
+    </>
   </React.StrictMode>
 )
